f1b752ccbc31d3aeac6b781b5c1c4a7bdbab23fe
abenaTenkorang <abenagy16@gmail.com>
2022-12-13 15:44:04 +0000

    Fix eslinters

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-﻿import './style.css';
+import './style.css';
 
 const Array = [
   {
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,21 @@
-﻿const path = require('path');
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 
 module.exports = {
   entry: './src/index.js',
-  mode: "development",
+  mode: 'development',
 
   devServer: {
     static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-     title: 'Output Management',
-     template: './src/index.html',
+      title: 'Output Management',
+      template: './src/index.html',
     }),
   ],
   output: {
-    filename: "[name].js",
+    filename: '[name].js',
 
     path: path.resolve(__dirname, 'dist'),
     clean: true,
