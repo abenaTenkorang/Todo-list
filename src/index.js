bca005bf8d6c3de21a779a25f52da3d5965d7157
abenaTenkorang <abenagy16@gmail.com>
2022-12-15 19:25:40 +0000

    Remove hard code

--- src/index.js
+++ src/index.js
@@ -1,42 +1,61 @@
+/* eslint-disable no-loop-func */
 import './style.css';
+import completeArray from './module/complete-array.js';
 
-const Array = [
-  {
-    description: 'washing dishes',
-    completed: 'false',
-    id: 1,
-  },
-  {
-    description: 'sweep the room',
-    completed: 'false',
-    id: 2,
-  },
-  {
-    description: 'eat',
-    completed: 'false',
-    id: 3,
-  },
-];
+let Array = [1];
+
 const container = document.querySelector('#list');
-for (let i = 0; i < Array.length; i += 1) {
-  const task = document.createElement('div');
-  task.classList.add('fisrt_todo');
-  container.append(task);
 
-  const checkbox = document.createElement('input');
-  checkbox.type = 'checkbox';
-  task.append(checkbox);
+const createTask = () => {
+  for (let i = 0; i < Array.length; i += 1) {
+    const task = document.createElement('div');
+    task.classList.add('fisrt_todo');
+    container.append(task);
 
-  const paragraph = document.createElement('p');
-  paragraph.classList.add('para');
-  paragraph.innerText = Array[i].description;
-  task.append(paragraph);
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    task.append(checkbox);
 
-  const optButton = document.createElement('button');
-  optButton.classList.add('optButton');
-  task.append(optButton);
+    const paragraph = document.createElement('p');
+    paragraph.classList.add('para');
+    paragraph.innerText = Array[i].description;
+    task.append(paragraph);
 
-  const optIcon = document.createElement('i');
-  optIcon.classList.add('fa-solid', 'fa-ellipsis-vertical');
-  optButton.append(optIcon);
-}
+    const optButton = document.createElement('button');
+    optButton.classList.add('optButton');
+    task.append(optButton);
+
+    const optIcon = document.createElement('i');
+    optIcon.classList.add('fa-solid', 'fa-ellipsis-vertical');
+    optButton.append(optIcon);
+    optButton.id = Array[i].id;
+
+    // task remove
+    const removeTask = () => {
+      const theTarget = optButton.id;
+      Array.splice(theTarget, 1);
+      let refreshId = 0;
+
+      if (Array.length > 0) {
+        Array.forEach((p) => {
+          p.id = refreshId;
+          refreshId += 1;
+        });
+      }
+    };
+    optButton.addEventListener('click', () => {
+      Array = JSON.parse(localStorage.getItem('Array')) || [];
+      removeTask();
+      localStorage.setItem('Array', JSON.stringify(Array));
+      window.location.reload();
+    });
+  }
+};
+
+const enterBtn = document.querySelector('.enter');
+enterBtn.addEventListener('click', () => {
+  completeArray(Array);
+  window.location.reload();
+});
+Array = JSON.parse(localStorage.getItem('Array')) || [];
+createTask();
